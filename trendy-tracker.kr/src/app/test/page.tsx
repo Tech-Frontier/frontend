22f6ac2c245f88284238b5ba6b4c754e94de2aa1
pongdang <pongpong0121@kakao.com>
2023-07-28 01:22:40 +0900

    feat: 예시 추가

--- trendy-tracker.kr/src/app/test/page.tsx
+++ trendy-tracker.kr/src/app/test/page.tsx
@@ -3,7 +3,12 @@ import { Text } from '@tech-frontier/shared-text';
 export default function Test() {
   return (
     <>
-      <Text />
+      <Text typography='Large'>하이</Text>
+      <Text typography='T1'>하이</Text>
+      <Text typography='T2'>하이</Text>
+      <Text typography='T3'>하이</Text>
+      <Text typography='Normal'>하이</Text>
+      <Text typography='Small'>하이</Text>
     </>
   );
 }
