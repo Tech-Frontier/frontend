82ba997a03a33274d13a794505136aa1e2739e74
dp <126376945+divopsor@users.noreply.github.com>
2023-08-13 02:22:52 +0900

    feat(admin): 라이브 환경의 서버 사이드에서 문제 확인 (#15)

--- admin.trendy-tracker.kr/src/app/AuthCheckServer.ts
+++ admin.trendy-tracker.kr/src/app/AuthCheckServer.ts
@@ -1,17 +1,19 @@
 import { headers } from 'next/headers';
-import { redirect } from 'next/navigation';
-import { request } from '@/utils/request/common';
+// import { redirect } from 'next/navigation';
+// import { request } from '@/utils/request/common';
 
 export async function AuthCheckServer() {
   const pathname = headers().get('x-invoke-path');
 
-  if (pathname !== '/login') {
-    try {
-      await request({ pathname: '/auth/health' });
-    } catch {
-      redirect('/login');
-    }
-  }
+  console.log({ pathname });
+
+  // if (pathname !== '/login') {
+  //   try {
+  //     await request({ pathname: '/auth/health' });
+  //   } catch {
+  //     redirect('/login');
+  //   }
+  // }
 
   return null;
 }
