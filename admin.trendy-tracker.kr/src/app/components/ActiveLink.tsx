b6429343120fe1a42f0ddc75ca3045c761f771bc
dp <126376945+divopsor@users.noreply.github.com>
2023-08-13 02:31:43 +0900

    feat(admin): 라우팅할 때 권한 체크 (#16)

--- admin.trendy-tracker.kr/src/app/components/ActiveLink.tsx
+++ admin.trendy-tracker.kr/src/app/components/ActiveLink.tsx
@@ -1,17 +1,31 @@
 'use client';
 
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
+import { request } from '@/utils/request/common';
 import { css } from '@styled-system/css';
 
 export function ActiveLink({ path, title }: { path: string; title: string; }) {
   const pathname = usePathname();
+  const router = useRouter();
 
   return (
-    <Link href={path} className={css({
+    <a className={css({
       fontWeight: pathname === path ? 'bold' : 'unset',
-    })}>
+      cursor: 'pointer',
+    })}
+    onClick={async () => {
+      (async () => {
+        try {
+          await request({ pathname: '/auth/health' });
+
+          router.push(path);
+        } catch {
+          router.push('/login');
+        }
+      })();
+    }}
+    >
       {title}
-    </Link>
+    </a>
   );
 }
