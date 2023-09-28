f5bdcf32e490e0f611543c45b4c1f1bff6452fe4
pongdang <pongpong0121@kakao.com>
2023-09-28 19:04:04 +0900

    fix

--- trendy-tracker.kr/src/components/Recruit/MainTitle.tsx
+++ trendy-tracker.kr/src/components/Recruit/MainTitle.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
-import Twemoji from '@/components/Twemoji';
 import { css } from '@/../styled-system/css';
+import Twemoji from '@/components/Twemoji';
 
 export function MainTitle({ emoji, children }: { emoji: string; children: ReactNode }) {
   return (
