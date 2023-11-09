807b589dbe7443691b47b9e88d6ebce355b7f7af
pongdang <pongpong0121@kakao.com>
2023-11-10 00:59:19 +0900

    refactor

--- trendy-tracker.kr/src/components/If.tsx
+++ trendy-tracker.kr/src/components/If.tsx
@@ -1,6 +1,8 @@
+import { ReactNode } from 'react';
+
 interface Props {
   condition: boolean;
-  children: React.ReactNode;
+  children: ReactNode;
 }
 export function If(props: Props) {
   return props.condition ? <>{props.children}</> : <></>;
