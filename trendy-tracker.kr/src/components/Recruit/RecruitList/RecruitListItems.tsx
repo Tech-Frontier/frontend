82c7a05f7dc6ddaba780cad56627904573347418
dp <126376945+divopsor@users.noreply.github.com>
2023-11-11 09:53:30 +0900

    feat(main): 공고 없음을 표기 (#36)

--- trendy-tracker.kr/src/components/Recruit/RecruitList/RecruitListItems.tsx
+++ trendy-tracker.kr/src/components/Recruit/RecruitList/RecruitListItems.tsx
@@ -1,3 +1,4 @@
+import { Text } from '@tech-frontier/ui-desktop';
 import { RecruitItem } from '@/components/Recruit';
 import { fetchRecruitList } from '@/utils/api/recruit';
 import type { RecruitItemData } from '@/types/Recruit/RecruitItemData';
@@ -6,6 +7,9 @@ export async function RecruitListItems({ tech }: { tech: string[] }) {
   const { data } = await fetchRecruitList({ tech });
   const { recruitList } = data;
 
+  if (recruitList.length === 0) {
+    return <Text color="white">등록된 공고가 없습니다.</Text>;
+  }
   // API 요청 예시 : https://api.trendy-tracker.kr/api/recruit/list?tech=Javascript&tech=react'
 
   return (
